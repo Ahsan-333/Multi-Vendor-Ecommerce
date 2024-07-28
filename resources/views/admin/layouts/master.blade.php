8893fb8e2be1ac75400ea12cd853ecb789a1d9c0
Ahsan-333 <ahsanrehman232@gmail.com>
2024-07-28 11:19:18 -0700

    Slider delete, half work completed

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -84,54 +84,93 @@
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script>
     $(document).ready(function(){
-      $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-      });
-
-      $('body').on('click', '.delete-item', function(event){
-        event.preventDefault();
-
-        let deleteUrl = $(this).attr('href');
-
-        Swal.fire({
-          title: "Are you sure?",
-          text: "You won't be able to revert this!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-          if (result.isConfirmed) {
-
-            $.ajax({
-              type: 'DELETE',
-              url: deleteUrl,
-              success: function(data){
-                if(data.status == 'success'){
-                  Swal.fire(
-                    "Deleted!",
-                    data.message,
-                  );
-                  window.location.reload();
-                }elseif(data.status == 'error'){
-                  Swal.fire(
-                    "Can't Delete!",
-                    data.message,
-                  );
-
-              },
-              error: function(xhr, status, error){
-              console.log(error)
-              }
-          })
-          }
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
         });
+        $('body').on('click', '.delete-item', function(event){
+            event.preventDefault();
+            let deleteUrl = $(this).attr('href')
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+                }).then((result) => {
+                if (result.isConfirmed) {
 
-      });
-    });
+                    $.ajax({
+                        type: 'DELETE',
+                        url : deleteUrl,
+                        success: function(data){
+                            Swal.fire({
+                            title: "Deleted!",
+                            text: "Your file has been deleted.",
+                            icon: "success"
+                            });
+                        },
+                        error: function(xhr, status, error){
+                            console.log(error);
+                        }
+                    })
+
+                }
+                });
+
+        })
+    })
+    // $(document).ready(function(){
+    //   $.ajaxSetup({
+    //     headers: {
+    //         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    //     }
+    //   });
+
+    //   $('body').on('click', '.delete-item', function(event){
+    //     event.preventDefault();
+
+    //     let deleteUrl = $(this).attr('href');
+
+    //     Swal.fire({
+    //       title: "Are you sure?",
+    //       text: "You won't be able to revert this!",
+    //       icon: "warning",
+    //       showCancelButton: true,
+    //       confirmButtonColor: "#3085d6",
+    //       cancelButtonColor: "#d33",
+    //       confirmButtonText: "Yes, delete it!"
+    //     }).then((result) => {
+    //       if (result.isConfirmed) {
+
+    //         $.ajax({
+    //           type: 'DELETE',
+    //           url: deleteUrl,
+    //           success: function(data){
+    //             if(data.status == 'success'){
+    //               Swal.fire(
+    //                 "Deleted!",
+    //                 data.message,
+    //               );
+    //               window.location.reload();
+    //             }elseif(data.status == 'error'){
+    //               Swal.fire(
+    //                 "Can't Delete!",
+    //                 data.message,
+    //               );
+    //             }
+    //           },
+    //           error: function(xhr, status, error){
+    //           console.log(error)
+    //           }
+    //       })
+    //       }
+    //     });
+    //   });
+    // });
   </script>
           @if ($errors->any())
             @foreach ($errors->all() as $error)
